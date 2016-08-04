2d4f071c8b84bd0b4e55934237c8eccc09b2cdac
snap492 <mysnap492@gmail.com>
2016-08-04 16:04:35 +0300

    new function getOrders

--- eshop/admin/orders.php
+++ eshop/admin/orders.php
@@ -2,6 +2,8 @@
 	require "secure/session.inc.php";
 	require "../inc/lib.inc.php";
 	require "../inc/db.inc.php";
+$allOrders= getOrders();
+
 ?>
 <html>
 <head>
@@ -10,16 +12,19 @@
 <body>
 <h1>Поступившие заказы:</h1>
 <?php
-
-?>
-<hr>
-<h2>Заказ номер: </h2>
-<p><b>Заказчик</b>: </p>
-<p><b>Email</b>: </p>
-<p><b>Телефон</b>: </p>
-<p><b>Адрес доставки</b>: </p>
-<p><b>Дата размещения заказа</b>: </p>
-
+foreach ($allOrders as $orders) {
+	?>
+	<hr>
+	<h2>Заказ номер:<?=$orders['orderid']?> </h2>
+	<p><b>Заказчик</b>:<?=$orders['name']?> </p>
+	<p><b>Email</b>:<?=$orders['email']?> </p>
+	<p><b>Телефон</b>:<?=$orders['phone']?> </p>
+	<p><b>Адрес доставки</b>: <?=$orders['address']?></p>
+	<p><b>Дата размещения заказа</b>:<?=$orders['dt']?> </p>
+	<?$cnt = 1;
+	foreach ($orders['goods'] as $goods){
+		var_dump($orders['goods']);
+		?>
 <h3>Купленные товары:</h3>
 <table border="1" cellpadding="5" cellspacing="0" width="90%">
 <tr>
@@ -30,10 +35,20 @@
 	<th>Цена, руб.</th>
 	<th>Количество</th>
 </tr>
+	<tr>
+	<td><?=$cnt++?></td>
+	<td><?=$goods['title']?></td>
+	<td><?=$goods['author']?></td>
+	<td><?=$goods['pubyear']?></td>
+	<td><?=$goods['price']?>, руб.</td>
+	<td><?=$goods['quantity']?></td>
+</tr>
 
 
 </table>
-<p>Всего товаров в заказе на сумму: руб.</p>
-
+<p>Всего товаров в заказе на сумму:<?=$goods['price']*$goods['quantity']?> руб.</p>
+	<?}
+}
+?>
 </body>
 </html>
